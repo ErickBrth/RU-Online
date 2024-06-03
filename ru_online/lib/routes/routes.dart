553fd990fce2bc8730c0f58e2bb6c608d278bfd8
ErickBrth <erickaraken@gmail.com>
2024-06-03 15:09:55 -0300

    update: add register page route

--- ru_online/lib/routes/routes.dart
+++ ru_online/lib/routes/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ru_online/presentation/login/pages/login.dart';
 import 'package:ru_online/presentation/navigation/pages/navigation.dart';
+import 'package:ru_online/presentation/register/pages/register_page.dart';
 
 final routes = GoRouter(routes: [
   GoRoute(
@@ -11,5 +12,9 @@ final routes = GoRouter(routes: [
   GoRoute(
     path: '/home',
     builder: (BuildContext context, GoRouterState state) => const Navigation(),
+  ),
+  GoRoute(
+    path: '/register',
+    builder: (BuildContext context, GoRouterState state) => const RegisterPage(),
   ),
 ]);
